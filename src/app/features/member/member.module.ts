bdfc6182e6ff3c9d9fbf8aa68ce84855937bda59
SAWADOGO Ahmad Abdoul Latif <sawahmad51@mail.com>
2026-05-05 13:46:49 +0000

    creation de groupe

--- src/app/features/member/member.module.ts
+++ src/app/features/member/member.module.ts
@@ -3,19 +3,21 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { LayoutModule }  from '../../layout/layout.module';
 
-import { MemberShellComponent }     from './member-shell/member-shell.component';
-import { MemberDashboardComponent } from './dashboard/member-dashboard.component';
-import { MyGroupsComponent }        from './my-groups/member-my-groups.component';
-import { PaymentComponent }         from './payment/member-payment.component';
-import { OrdersComponent }          from './orders/member-orders.component';
-import { NotificationsComponent }   from './notifications/member-notifications.component';
-import { ProfileComponent }         from './profile/member-profile.component';
+import { MemberShellComponent }      from './member-shell/member-shell.component';
+import { MemberDashboardComponent }  from './dashboard/member-dashboard.component';
+import { MyGroupsComponent }         from './my-groups/member-my-groups.component';
+import { MemberCatalogueComponent }  from './catalogue/member-catalogue.component';
+import { PaymentComponent }          from './payment/member-payment.component';
+import { OrdersComponent }           from './orders/member-orders.component';
+import { NotificationsComponent }    from './notifications/member-notifications.component';
+import { ProfileComponent }          from './profile/member-profile.component';
 
 @NgModule({
   declarations: [
     MemberShellComponent,
     MemberDashboardComponent,
     MyGroupsComponent,
+    MemberCatalogueComponent,
     PaymentComponent,
     OrdersComponent,
     NotificationsComponent,
@@ -31,6 +33,7 @@ import { ProfileComponent }         from './profile/member-profile.component';
         children: [
           { path: '',              component: MemberDashboardComponent },
           { path: 'groups',        component: MyGroupsComponent },
+          { path: 'catalogue',     component: MemberCatalogueComponent },
           { path: 'payment',       component: PaymentComponent },
           { path: 'orders',        component: OrdersComponent },
           { path: 'notifications', component: NotificationsComponent },
