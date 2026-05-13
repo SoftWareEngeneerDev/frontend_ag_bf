0bc8654e26f457ecef2416d33867aa433beba77f
SoftWareEngeneerDev <mailforlearning19@gmail.com>
2026-05-13 06:05:26 +0000

    Fix API URL production - use environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,18 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:3000/api/v1',
+//   socketUrl: 'http://localhost:3000',
+//   appName: 'AchatGroupéBF',
+//   currency: 'XOF',
+//   depositPercent: 10,
+//   commissionPercent: 7,
+// };
+
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1',
-  socketUrl: 'http://localhost:3000',
+  apiUrl: 'https://back-achat-group-production.up.railway.app/api/v1',
+  socketUrl: 'https://back-achat-group-production.up.railway.app',
   appName: 'AchatGroupéBF',
   currency: 'XOF',
   depositPercent: 10,
